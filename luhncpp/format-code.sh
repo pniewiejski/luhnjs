cf44bb0f44a7a6665cabc0f515ee53f47db3dacf
Piotr Niewiejski <piotrniewiejski@gmail.com>
2020-03-20 22:42:35 +0100

    Update cpp formating script to use basic regex in find command
    - This commit changes the regex used in the find command in the
      script that calls clang-format. This is due to the fact that the
      find command on BSD does not have extened regex out of the box.
    
      More information here:
      https://stackoverflow.com/questions/5905493/why-isnt-this-regex-working-find-regex-m-h
    
    - Remove `cu` file extension in find regex as it is not used
      in this project.

--- luhncpp/format-code.sh
+++ luhncpp/format-code.sh
@@ -1,5 +1,5 @@
 set -xe
 for DIR in ./src ./include ./test
 do
-find $DIR -regex '.*\.\(cpp\|hpp\|cu\|c\|h\)' -exec clang-format -style=file -i {} \;
+    find $DIR -regex '.*\.[ch]p*' -exec clang-format -style=file -i {} \;
 done 
